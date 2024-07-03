7be5b88fc8c4415fbbb12a4b4b7b7ddb4c42b7b0
navaneethan077 <navaneethansivakumaran@gmail.com>
2024-07-03 09:24:44 +0530

    database model

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,12 +1,48 @@
+import Movie from "../models/movie.model.js";
+
  export const MovieIndex = (req , res) => {
     res.send("get all movies lists");
 };
 
-export const MovieCreate = (req , res) => {
-    console.log(req.body);
+// export const MovieCreate = async(req , res) => {
+//     // id ,title ,dec
+//     // console.log(req.body);
 
-    return res.json(req.body);
-};
+//     //validate your data
+//     const newMovie = new Movies({
+//         title: req.body.title,
+//         desc: req.body.desc,
+//     });
+
+//     try {
+//         const movie = await newMovie.save(); 
+//         return res.status(201).json(movie);
+           
+//     } catch (error) {
+//         return res.status(400).json({message: error.massage});
+        
+//     }
+
+//     // const movie = await newMovie.save(); 
+
+//     // return res.json(req.body);
+// };
+export const MovieCreate = async (req, res) => {
+    // id, title, desc
+    // Validate your data.
+    const newMovie = new Movie({
+      title: req.body.title,
+      desc: req.body.desc,
+    });
+  
+    try {
+      const movie = await newMovie.save();
+      return res.status(201).json(movie);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  };
+  
 
 export const MovieUpdate = (req , res) => {
     res.send("update movies");
